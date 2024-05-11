8bf7403e51957eb1df38076dd8d8c0f77d87926b
Fardin15 <mohammedshowrav15@gmail.com>
2024-05-11 22:33:04 +0600

    get signle jobs data

--- index.js
+++ index.js
@@ -37,12 +37,12 @@ async function run() {
     });
 
     // get a single jobs data
-    // app.get("/job/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await jobsCollection.findOne(query);
-    //   res.send(result);
-    // });
+    app.get("/job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
