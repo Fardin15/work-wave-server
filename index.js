49ee36f5e2264efae30ce3602bf0a862a5d1b21e
Fardin15 <mohammedshowrav15@gmail.com>
2024-05-13 02:36:43 +0600

    working on filter and search bar

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 8000;
@@ -7,12 +9,39 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
-  Credential: true,
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "http://localhost:5176",
+    "http://localhost:5177",
+  ],
+  credential: true,
   optionSuccessStatus: 200,
 };
+
+// middlewares
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
+
+// verify jwt middleware
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  if (token) {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+      if (error) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      console.log(decoded);
+      req.user = decoded;
+      next();
+    });
+  }
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lnrcai2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -30,6 +59,34 @@ async function run() {
     // collections
     const jobsCollection = client.db("workWave").collection("jobs");
     const appliedCollection = client.db("workWave").collection("appliedJobs");
+
+    // jwt generator
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+          maxAge: 60 * 60 * 1000,
+        })
+        .send({ success: true });
+    });
+
+    // clear toke on log out
+    app.get("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 0,
+        })
+        .send({ success: true });
+    });
 
     // get all jobs data
     app.get("/jobs", async (req, res) => {
@@ -94,8 +151,20 @@ async function run() {
     app.get("/applied-jobs/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
-      const result = await appliedCollection.find(query).toArray();
+      const filter = req.query.filter;
+      let filterQuery = {};
+      if (filter) filterQuery = { category: filter };
+      const result = await appliedCollection.find(query, filterQuery).toArray();
       res.send(result);
+    });
+
+    // get data for filter by category
+    app.get("/job-counts", async (req, res) => {
+      const filter = req.query.filter;
+      let query = {};
+      if (filter) query = { category: filter };
+      const count = await appliedCollection.countDocuments(query);
+      res.send({ count });
     });
 
     // Send a ping to confirm a successful connection
